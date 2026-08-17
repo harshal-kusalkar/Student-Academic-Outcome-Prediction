bff2020e501ae41c20c4f715ab58da43e7926882
harshal_kusalkar <h41497254@gmail.com>
2026-08-17 18:40:24 +0530

    refactor: Configuration changed

--- entity/config_entity.py
+++ entity/config_entity.py
@@ -1,103 +1,82 @@
-from pathlib import Path
-from typing import Any
+from typing import Literal
 
 from pydantic import BaseModel
 
 
-# -----------------------------
-# Preprocessing
-# -----------------------------
+class DataPathsConfig(BaseModel):
+    data_path: str
+
+
+class TargetConfig(BaseModel):
+    name: str
+    classes: list[str]
+
+
+class ImputerConfig(BaseModel):
+    strategy: Literal[
+        "mean",
+        "median",
+        "most_frequent",
+        "constant",
+    ]
+
+
+class ScalerConfig(BaseModel):
+    type: Literal[
+        "standard",
+        "minmax",
+        "robust",
+        "none",
+    ]
+
+
+class EncoderConfig(BaseModel):
+    type: Literal[
+        "onehot",
+        "ordinal",
+    ]
+
+    handle_unknown: Literal[
+        "ignore",
+        "error",
+    ] = "ignore"
+
+
+class NumericalPreprocessingConfig(BaseModel):
+    imputer: ImputerConfig
+    scaler: ScalerConfig
+
+
+class CategoricalPreprocessingConfig(BaseModel):
+    imputer: ImputerConfig
+    encoder: EncoderConfig
+
+
+class BinaryPreprocessingConfig(BaseModel):
+    imputer: ImputerConfig
+
 
 class PreprocessingConfig(BaseModel):
-    drop_columns: list[str]
-    target_col: str
-    target: list[str]
+    numerical: NumericalPreprocessingConfig
+    categorical: CategoricalPreprocessingConfig
+    binary: BinaryPreprocessingConfig
 
 
-# -----------------------------
-# Data Split
-# -----------------------------
+class FeaturesConfig(BaseModel):
+    numerical: list[str]
+    categorical: list[str]
+    binary: list[str]
+
 
 class DataSplitConfig(BaseModel):
     test_size: float
     random_state: int
+    stratify: bool
 
-
-# -----------------------------
-# Paths
-# -----------------------------
-
-class DataPathConfig(BaseModel):
-    data_dir_path: Path
-
-
-class ArtifactPathConfig(BaseModel):
-    model_path: Path
-    encoder_path: Path
-    feature_names_path: Path
-    data_validation_path: Path
-    eval_result_path: Path
-    classification_report_path: Path
-
-
-class ProcessedDataConfig(BaseModel):
-    X_train_path: Path
-    X_test_path: Path
-    y_train_path: Path
-    y_test_path: Path
-
-
-# -----------------------------
-# Model
-# -----------------------------
-
-class ModelConfig(BaseModel):
-    name: str
-    version: str
-
-
-class TrainingConfig(BaseModel):
-    random_state: int
-    n_jobs: int
-
-
-class ModelParamsConfig(BaseModel):
-    objective: str = "multi:softprob"
-    eval_metric: str = "mlogloss"
-    booster: str = "gbtree"
-    verbosity: int = 0
-
-    n_estimators: int
-    learning_rate: float
-    max_depth: int
-    min_child_weight: float
-    subsample: float
-    colsample_bytree: float
-    gamma: float                       # Formerly min_split_gain
-    reg_alpha: float
-    reg_lambda: float
-
-
-class ExperimentConfig(BaseModel):
-    model_name: str = "XGBoost"
-    best_trial: int
-    cv_metric: str
-    best_cv_score: float
-
-
-# -----------------------------
-# Root Config
-# -----------------------------
 
 class Config(BaseModel):
-    preprocessing: PreprocessingConfig
-    data_split: DataSplitConfig
-    data_paths: DataPathConfig
-
-    artifacts: ArtifactPathConfig
-    processed_data: ProcessedDataConfig
-
-    model: ModelConfig
-    training: TrainingConfig
-    model_params: ModelParamsConfig
-    experiment: ExperimentConfig
+    DATA_PATHS: DataPathsConfig
+    TARGET: TargetConfig
+    FEATURES: FeaturesConfig
+    PREPROCESSING: PreprocessingConfig
+    DATA_SPLIT: DataSplitConfig
