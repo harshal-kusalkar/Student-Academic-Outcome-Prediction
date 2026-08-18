3722fa7ea15e30ac7dcf55b9c64ead16626e89a2
harshal_kusalkar <h41497254@gmail.com>
2026-08-18 19:47:40 +0530

    feat: added data split functionality.

--- src/data/split.py
+++ src/data/split.py
@@ -1,36 +1,95 @@
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 
 from utils.logger import get_logger
-from utils.load_config import load_config
-import pandas as pd 
-import numpy as np 
+
 
 logger = get_logger(__name__)
 
-def split_data(config, df):
 
-    logger.info("data split start:")
-    TEST_SIZE = config.data_split.test_size
-    RANDOM_STATE = config.data_split.random_state
-    target = config.preprocessing.target_col
-    logger.debug(f"test size: {TEST_SIZE}, random_state: {RANDOM_STATE}")
-    
-    X = df.drop(columns=target)
-    y = df[target]
+def split_data(
+    df: pd.DataFrame,
+    config,
+):
+    """
+    Split dataset into training and testing sets.
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,
-        y,
-        test_size=TEST_SIZE,
-        shuffle=True,
-        random_state=RANDOM_STATE
+    Returns:
+        X_train
+        X_test
+        y_train
+        y_test
+    """
+
+    logger.info("Data splitting started.")
+
+    target_col = config.TARGET.name
+
+    test_size = config.DATA_SPLIT.test_size
+    random_state = config.DATA_SPLIT.random_state
+
+    # -----------------------------------------
+    # Separate features and target
+    # -----------------------------------------
+
+    X = df.drop(
+        columns=[target_col]
     )
-    
-    logger.info(f"Training and test data created")
-    logger.debug(f"X_train, y_train: {X_train.shape, y_train.shape} | X_test, y_test: {X_test.shape, y_test.shape}")
 
-    return X_train, X_test, y_train, y_test
+    y = df[target_col]
 
+    logger.info(
+        "Features shape: %s",
+        X.shape,
+    )
 
+    logger.info(
+        "Target shape: %s",
+        y.shape,
+    )
 
-    
+    # -----------------------------------------
+    # Train/Test split
+    # -----------------------------------------
+
+    X_train, X_test, y_train, y_test = (
+        train_test_split(
+            X,
+            y,
+            test_size=test_size,
+            random_state=random_state,
+            stratify=y,
+        )
+    )
+
+    logger.info(
+        "X_train shape: %s",
+        X_train.shape,
+    )
+
+    logger.info(
+        "X_test shape: %s",
+        X_test.shape,
+    )
+
+    logger.info(
+        "y_train shape: %s",
+        y_train.shape,
+    )
+
+    logger.info(
+        "y_test shape: %s",
+        y_test.shape,
+    )
+
+    logger.info(
+        "Data splitting completed."
+    )
+
+    return (
+        X_train,
+        X_test,
+        y_train,
+        y_test,
+    )
