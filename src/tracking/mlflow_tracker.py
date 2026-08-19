6afc08093d162585e6493da1254d1ab62d3c78d8
harshal_kusalkar <h41497254@gmail.com>
2026-08-19 18:51:29 +0530

    feat: added mlflow tracking.

---
+++ src/tracking/mlflow_tracker.py
@@ -0,0 +1,85 @@
+import mlflow
+
+from utils.logger import get_logger
+
+
+logger = get_logger(__name__)
+
+
+def setup_mlflow(config):
+    """
+    Configure MLflow tracking.
+    """
+
+    mlflow.set_tracking_uri(
+        config.mlflow.tracking_uri
+    )
+
+    mlflow.set_experiment(
+        config.mlflow.experiment_name
+    )
+
+    logger.info(
+        "MLflow configured."
+    )
+
+    logger.info(
+        "Tracking URI: %s",
+        config.mlflow.tracking_uri,
+    )
+
+    logger.info(
+        "Experiment: %s",
+        config.mlflow.experiment_name,
+    )
+
+
+def log_model_run(
+    model_name,
+    model,
+    metrics,
+):
+    """
+    Log one model comparison experiment.
+    """
+
+    with mlflow.start_run(
+        run_name=model_name
+    ):
+
+        # -----------------------------
+        # Model information
+        # -----------------------------
+
+        mlflow.set_tag(
+            "model_name",
+            model_name,
+        )
+
+        mlflow.set_tag(
+            "stage",
+            "model_comparison",
+        )
+
+        # -----------------------------
+        # Parameters
+        # -----------------------------
+
+        model_params = model.get_params()
+
+        mlflow.log_params(
+            model_params
+        )
+
+        # -----------------------------
+        # Metrics
+        # -----------------------------
+
+        mlflow.log_metrics(
+            metrics
+        )
+
+        logger.info(
+            "MLflow run logged: %s",
+            model_name,
+        )
