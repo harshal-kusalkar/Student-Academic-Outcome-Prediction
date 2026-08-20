e10a2f7940426a824c83e8e5cf61e4e756853d03
harshal_kusalkar <h41497254@gmail.com>
2026-08-20 19:50:02 +0530

    feat: api implamented

---
+++ api/app.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+from fastapi import FastAPI
+
+from api.mappings import COLUMN_MAPPING
+from api.schemas import StudentPredictionRequest
+from src.inference.predictor import Predictor
+
+from utils.load_config import load_config
+from utils.logger import get_logger
+
+
+logger = get_logger(__name__)
+
+
+app = FastAPI(
+    title="Student Dropout Prediction API",
+    description=(
+        "ML inference API for predicting "
+        "student academic outcomes."
+    ),
+    version="1.0.0",
+)
+
+
+config = load_config()
+
+predictor = Predictor(
+    tracking_uri=config.mlflow.tracking_uri,
+    model_name=config.mlflow.registered_model_name,
+    alias=config.mlflow.model_alias,
+)
+
+
+@app.get("/health")
+def health_check():
+
+    return {
+        "status": "healthy",
+        "model": config.mlflow.registered_model_name,
+        "alias": config.mlflow.model_alias,
+    }
+
+
+@app.post("/predict")
+def predict(
+    request: StudentPredictionRequest,
+):
+
+    # Pydantic model → dictionary
+    data = request.model_dump()
+
+    # API names → dataset names
+    data = {
+        COLUMN_MAPPING[key]: value
+        for key, value in data.items()
+    }
+
+    # Dictionary → DataFrame
+    input_data = pd.DataFrame(
+        [data]
+    )
+
+    prediction = predictor.predict(
+        input_data
+    )
+
+    return {
+        "prediction": str(prediction[0])
+    }
---
+++ api/mappings.py
@@ -0,0 +1,83 @@
+COLUMN_MAPPING = {
+    "marital_status": "Marital status",
+    "application_mode": "Application mode",
+    "application_order": "Application order",
+    "course": "Course",
+    "daytime_evening_attendance": (
+        "Daytime/evening attendance"
+    ),
+    "previous_qualification": (
+        "Previous qualification"
+    ),
+    "previous_qualification_grade": (
+        "Previous qualification (grade)"
+    ),
+    "nationality": "Nacionality",
+    "mothers_qualification": (
+        "Mother's qualification"
+    ),
+    "fathers_qualification": (
+        "Father's qualification"
+    ),
+    "mothers_occupation": (
+        "Mother's occupation"
+    ),
+    "fathers_occupation": (
+        "Father's occupation"
+    ),
+    "admission_grade": "Admission grade",
+    "displaced": "Displaced",
+    "educational_special_needs": (
+        "Educational special needs"
+    ),
+    "debtor": "Debtor",
+    "tuition_fees_up_to_date": (
+        "Tuition fees up to date"
+    ),
+    "gender": "Gender",
+    "scholarship_holder": "Scholarship holder",
+    "age_at_enrollment": "Age at enrollment",
+    "international": "International",
+
+    "curricular_units_1st_sem_credited": (
+        "Curricular units 1st sem (credited)"
+    ),
+    "curricular_units_1st_sem_enrolled": (
+        "Curricular units 1st sem (enrolled)"
+    ),
+    "curricular_units_1st_sem_evaluations": (
+        "Curricular units 1st sem (evaluations)"
+    ),
+    "curricular_units_1st_sem_approved": (
+        "Curricular units 1st sem (approved)"
+    ),
+    "curricular_units_1st_sem_grade": (
+        "Curricular units 1st sem (grade)"
+    ),
+    "curricular_units_1st_sem_without_evaluations": (
+        "Curricular units 1st sem (without evaluations)"
+    ),
+
+    "curricular_units_2nd_sem_credited": (
+        "Curricular units 2nd sem (credited)"
+    ),
+    "curricular_units_2nd_sem_enrolled": (
+        "Curricular units 2nd sem (enrolled)"
+    ),
+    "curricular_units_2nd_sem_evaluations": (
+        "Curricular units 2nd sem (evaluations)"
+    ),
+    "curricular_units_2nd_sem_approved": (
+        "Curricular units 2nd sem (approved)"
+    ),
+    "curricular_units_2nd_sem_grade": (
+        "Curricular units 2nd sem (grade)"
+    ),
+    "curricular_units_2nd_sem_without_evaluations": (
+        "Curricular units 2nd sem (without evaluations)"
+    ),
+
+    "unemployment_rate": "Unemployment rate",
+    "inflation_rate": "Inflation rate",
+    "gdp": "GDP",
+}
